8ef195a51ba06c602f2ef8b9a06fc89a84a570f1
Mati trabajo <matubianchi@gmail.com>
2022-11-01 13:55:11 -0300

    cambios

--- javascript/torneos.js
+++ javascript/torneos.js
@@ -1,104 +1,101 @@
-PARTIDOMUNSTORAGE = localStorage.getItem('partidoMunstore');
+PARTIDOMUNSTORAGE = localStorage.getItem("partidoMunstore");
 const PARTIDOMUN = JSON.parse(PARTIDOMUNSTORAGE) ?? [];
 
-const PARTIDOSTORAGE = localStorage.getItem('PartidoStorage');
+const PARTIDOSTORAGE = localStorage.getItem("PartidoStorage");
 const PARTIDOUSUARIO = JSON.parse(PARTIDOSTORAGE) ?? [];
 
-const USERSTORAGE = localStorage.getItem('user');
-function redirect(){
-    window.location='./login.html'
+const USERSTORAGE = localStorage.getItem("user");
+function redirect() {
+  window.location = "./login.html";
 }
 const USER = JSON.parse(USERSTORAGE) ?? redirect();
 
-document.getElementById('navbarDropdownMenuLink').innerHTML=USER.username
-const logout = document.getElementById('logout')
-logout.addEventListener("click", e=>{
-    localStorage.removeItem('user')
-    redirect()
-})
+document.getElementById("navbarDropdownMenuLink").innerHTML = USER.username;
+const logout = document.getElementById("logout");
+logout.addEventListener("click", (e) => {
+  localStorage.removeItem("user");
+  redirect();
+});
 
-
-puntosGanados = 3
-puntosGoles = 1
+puntosGanados = 3;
+puntosGoles = 1;
 
 let TORNEOGENERAL = [];
-let TORNEOSIND = []
-let USUARIOS = []
+let TORNEOSIND = [];
+let USUARIOS = [];
 
-
-
-const tabla = document.getElementById('tabla');
-
+const tabla = document.getElementById("tabla");
 
 /////////////////////////////ENVIO DE RESULTADOS///////////////////////////
 
 let PUNTOS = 0;
 
-
 PARTIDOMUN.forEach((partidomun, i) => {
-    Pts = 0
-    if (partidomun.terminado == true) {
-        if (partidomun.geq1>partidomun.geq2 && PARTIDOUSUARIO[i].geq1>PARTIDOUSUARIO[i].geq2) {
-            PUNTOS += puntosGanados;
-            Pts += puntosGanados;
-        } else if (partidomun.geq1==partidomun.geq2 && PARTIDOUSUARIO[i].geq1 == PARTIDOUSUARIO[i].geq2) {
-            PUNTOS += puntosGanados;
-            Pts += puntosGanados;
-        } else if (partidomun.geq1<partidomun.geq2 && PARTIDOUSUARIO[i].geq1<PARTIDOUSUARIO[i].geq2) {
-            PUNTOS += puntosGanados;
-            Pts += puntosGanados;
-        }
-        if (partidomun.geq1==PARTIDOUSUARIO[i].geq1){
-            PUNTOS += puntosGoles;
-            Pts += puntosGoles;
-        }
-        if (partidomun.geq2==PARTIDOUSUARIO[i].geq2){
-            PUNTOS += puntosGoles;
-            Pts += puntosGoles;
-        }
+  Pts = 0;
+  if (partidomun.terminado == true) {
+    if (
+      partidomun.geq1 > partidomun.geq2 &&
+      PARTIDOUSUARIO[i].geq1 > PARTIDOUSUARIO[i].geq2
+    ) {
+      PUNTOS += puntosGanados;
+      Pts += puntosGanados;
+    } else if (
+      partidomun.geq1 == partidomun.geq2 &&
+      PARTIDOUSUARIO[i].geq1 == PARTIDOUSUARIO[i].geq2
+    ) {
+      PUNTOS += puntosGanados;
+      Pts += puntosGanados;
+    } else if (
+      partidomun.geq1 < partidomun.geq2 &&
+      PARTIDOUSUARIO[i].geq1 < PARTIDOUSUARIO[i].geq2
+    ) {
+      PUNTOS += puntosGanados;
+      Pts += puntosGanados;
     }
+    if (partidomun.geq1 == PARTIDOUSUARIO[i].geq1) {
+      PUNTOS += puntosGoles;
+      Pts += puntosGoles;
+    }
+    if (partidomun.geq2 == PARTIDOUSUARIO[i].geq2) {
+      PUNTOS += puntosGoles;
+      Pts += puntosGoles;
+    }
+  }
 
-    const res = document.createElement('tr');
-        if (partidomun.terminado == true) {
-            res.innerHTML = `
+  const res = document.createElement("tr");
+  if (partidomun.terminado == true) {
+    res.innerHTML = `
             <td>${partidomun.eq1.nombre} || ${partidomun.geq1}-${partidomun.geq2} || ${partidomun.eq2.nombre}</td>
             <td>${PARTIDOUSUARIO[i].eq1.nombre} || ${PARTIDOUSUARIO[i].geq1}-${PARTIDOUSUARIO[i].geq2} || ${PARTIDOUSUARIO[i].eq2.nombre}</td>
             <td class="usupuntos">${Pts}</td>
-            <td class="usupuntos">${PUNTOS}</td>`
-            tabla.prepend(res)
-        }
-        
-    
+            <td class="usupuntos">${PUNTOS}</td>`;
+    tabla.prepend(res);
+  }
 });
 
 let puntostotal = document.getElementById("spanpuntos");
-puntostotal.innerHTML = PUNTOS
-
+puntostotal.innerHTML = PUNTOS;
 
 document.getElementById("tablaUsuario").addEventListener("click", (e) => {
-
-    document.getElementById("collapse").classList.toggle("show");
-    const s = document.querySelector(".flechita");
-    if (s.classList.contains("fa-caret-down")){
-        s.classList.remove("fa-caret-down")
-        s.classList.add("fa-caret-up")
-    } else {
-        s.classList.remove("fa-caret-up")
-        s.classList.add("fa-caret-down")
-    }
-    
-})
-
-
+  document.getElementById("collapse").classList.toggle("show");
+  const s = document.querySelector(".flechita");
+  if (s.classList.contains("fa-caret-down")) {
+    s.classList.remove("fa-caret-down");
+    s.classList.add("fa-caret-up");
+  } else {
+    s.classList.remove("fa-caret-up");
+    s.classList.add("fa-caret-down");
+  }
+});
 
 class Usuarios {
-    constructor(nombre, usuario, puntos, mail, id) {
-        this.nombre = nombre;
-        this.usuario = usuario;
-        this.puntos = puntos;
-        this.mail = mail;
-        this.id = id;
-}
+  constructor(nombre, usuario, puntos, mail, id) {
+    this.nombre = nombre;
+    this.usuario = usuario;
+    this.puntos = puntos;
+    this.mail = mail;
+    this.id = id;
+  }
 }
 
 const mati = new Usuarios("Mati", "matute", PUNTOS, "matubianchi@gmail.com", 1);
@@ -117,91 +114,86 @@ const leti = new Usuarios("leti", "leti", 110, "bla@gmail.com", 8);
 const lore = new Usuarios("lore", "lore", 112, "bla@gmail.com", 8);
 const toto = new Usuarios("toto", "toto", 127, "bla@gmail.com", 8);
 
-
-
-USUARIOS.push(mati)
-USUARIOS.push(gudi)
-USUARIOS.push(marcos)
-USUARIOS.push(pablo)
-USUARIOS.push(andres)
-USUARIOS.push(juani)
-USUARIOS.push(juampa)
-USUARIOS.push(lucas)
-USUARIOS.push(majo)
-USUARIOS.push(gon)
-USUARIOS.push(tom)
-USUARIOS.push(tincho)
-USUARIOS.push(leti)
-USUARIOS.push(lore)
-USUARIOS.push(toto)
-
+USUARIOS.push(mati);
+USUARIOS.push(gudi);
+USUARIOS.push(marcos);
+USUARIOS.push(pablo);
+USUARIOS.push(andres);
+USUARIOS.push(juani);
+USUARIOS.push(juampa);
+USUARIOS.push(lucas);
+USUARIOS.push(majo);
+USUARIOS.push(gon);
+USUARIOS.push(tom);
+USUARIOS.push(tincho);
+USUARIOS.push(leti);
+USUARIOS.push(lore);
+USUARIOS.push(toto);
 
 class TorneoInd {
-    constructor(nombre, usuarios, id) {
-        this.nombre = nombre;
-        this.usuarios = usuarios;
-        this.id = id;
-    }
+  constructor(nombre, usuarios, id) {
+    this.nombre = nombre;
+    this.usuarios = usuarios;
+    this.id = id;
+  }
 }
 
-const Pibes = new TorneoInd("Pibes", [mati, gudi, pablo, andres, juani, lucas, juampa],1)
-const Laburo = new TorneoInd("Laburo", [mati, tincho, leti, lore],2)
-const Flia = new TorneoInd("Flia", [mati, majo, gon, tom],3)
-const Coderhouse = new TorneoInd("Coderhouse", [mati, toto, andres],4)
+const Pibes = new TorneoInd(
+  "Pibes",
+  [mati, gudi, pablo, andres, juani, lucas, juampa],
+  1
+);
+const Laburo = new TorneoInd("Laburo", [mati, tincho, leti, lore], 2);
+const Flia = new TorneoInd("Flia", [mati, majo, gon, tom], 3);
+const Coderhouse = new TorneoInd("Coderhouse", [mati, toto, andres], 4);
 
 TORNEOSIND.push(Pibes);
 TORNEOSIND.push(Laburo);
 TORNEOSIND.push(Flia);
 TORNEOSIND.push(Coderhouse);
 
-
 let torneoSort = () => {
-    TORNEOSIND.forEach(torneo => {
-        torneo.usuarios.sort((a, b) => b.puntos - a.puntos)
-        })
-    }
-
-
+  TORNEOSIND.forEach((torneo) => {
+    torneo.usuarios.sort((a, b) => b.puntos - a.puntos);
+  });
+};
 
 // TOR = TORNEOSIND.usuarios;
 let cargaTorneosInd = () => {
-TORNEOSIND.forEach((torneo) => {
-    const tabla = document.getElementById('torneoindividual')
-    const tablaind = document.createElement('tbody')
+  TORNEOSIND.forEach((torneo) => {
+    const tabla = document.getElementById("torneoindividual");
+    const tablaind = document.createElement("tbody");
     tablaind.id = torneo.nombre;
-    tabla.appendChild(tablaind)
-    torneo.usuarios.forEach((usuario,index) => {
-        crearTablaTorneoPrin(usuario, `${torneo.nombre}`,index)
+    tabla.appendChild(tablaind);
+    torneo.usuarios.forEach((usuario, index) => {
+      crearTablaTorneoPrin(usuario, `${torneo.nombre}`, index);
     });
-    tablaind.style.display = 'none';
-    tablaind.value = 'torneosind'
-
-});
-}
+    tablaind.style.display = "none";
+    tablaind.value = "torneosind";
+  });
+};
 
 const agregarEventListener = () => {
-const select = document.getElementById('fstdiv');
+  const select = document.getElementById("fstdiv");
 
-select.addEventListener('change', (e) => {
-    var selecte = document.getElementById('fstdiv');
+  select.addEventListener("change", (e) => {
+    var selecte = document.getElementById("fstdiv");
     var value = selecte.options[select.selectedIndex].value;
-    
 
-    const GRUPOSTOGGLE = TORNEOSIND
+    const GRUPOSTOGGLE = TORNEOSIND;
 
-    const newGrupo = GRUPOSTOGGLE.filter(gr => {
-        return gr.nombre !== value
-    })
-    newGrupo.forEach(tabla => {
-        document.getElementById(tabla.nombre).style.display = 'none'
-
+    const newGrupo = GRUPOSTOGGLE.filter((gr) => {
+      return gr.nombre !== value;
     });
-    document.getElementById(value).style.display = ''
-})
-}
+    newGrupo.forEach((tabla) => {
+      document.getElementById(tabla.nombre).style.display = "none";
+    });
+    document.getElementById(value).style.display = "";
+  });
+};
 
-console.log("Hecho por Matias Bianchi")
-console.log("https://github.com/matibian/Fixture-Qatar-22")
+console.log("Hecho por Matias Bianchi");
+console.log("https://github.com/matibian/Fixture-Qatar-22");
 
 // const API_URL = 'https://prode-qatar-default-rtdb.firebaseio.com/Torneos.json'
 
@@ -210,122 +202,138 @@ console.log("https://github.com/matibian/Fixture-Qatar-22")
 //     .then((response) => response.json())
 //     .then((data) => {
 //         TORNEOSIND = data.Torneos
-        // cargaTorneosInd()
-        // mostrarTorneo()
-        // agregarEventListener()
-        // USUARIOS.forEach((usuario,index) => {
-        //     crearTablaTorneoPrin(usuario, 'tabgeneral',index)
-        // });
-        // torneoSort()
-        
+// cargaTorneosInd()
+// mostrarTorneo()
+// agregarEventListener()
+// USUARIOS.forEach((usuario,index) => {
+//     crearTablaTorneoPrin(usuario, 'tabgeneral',index)
+// });
+// torneoSort()
+
 //     })
-    
+
 // }
 // torneosDesdeBD()
 
-
-
-
-
-
-
-
 function crearTablaTorneoPrin(u, nom, index) {
+  const tabla = document.getElementById(nom);
 
-    
-    const tabla = document.getElementById(nom)
+  let tr = document.createElement("tr");
+  let td1 = document.createElement("td");
+  td1.className = "tabGenUsuario";
+  td1.innerHTML = `${index + 1}º`;
+  let td2 = document.createElement("td");
+  td2.className = "tabGenUsuario";
+  td2.innerHTML = u.usuario;
+  let td3 = document.createElement("td");
+  td3.className = "tabGenUsuario";
+  td3.innerHTML = u.puntos;
 
-    let tr = document.createElement('tr');
-    let td1 = document.createElement('td');
-    td1.className = 'tabGenUsuario'
-    td1.innerHTML = `${index+1}º`;
-    let td2 = document.createElement('td');
-    td2.className = 'tabGenUsuario'
-    td2.innerHTML = u.usuario;
-    let td3 = document.createElement('td');
-    td3.className = 'tabGenUsuario'
-    td3.innerHTML = u.puntos;
+  tr.appendChild(td1);
+  tr.appendChild(td2);
+  tr.appendChild(td3);
 
-    tr.appendChild(td1);
-    tr.appendChild(td2);
-    tr.appendChild(td3);
-
-    tabla.appendChild(tr);
-
+  tabla.appendChild(tr);
 }
 
-USUARIOS.sort((a, b) => b.puntos - a.puntos)
-TORNEOSIND.forEach(torneo => {
-    torneo.usuarios.sort((a, b) => b.puntos - a.puntos)
-
+USUARIOS.sort((a, b) => b.puntos - a.puntos);
+TORNEOSIND.forEach((torneo) => {
+  torneo.usuarios.sort((a, b) => b.puntos - a.puntos);
 });
 
-USUARIOS.forEach((usuario,index) => {
-    crearTablaTorneoPrin(usuario, 'tabgeneral',index)
+USUARIOS.forEach((usuario, index) => {
+  crearTablaTorneoPrin(usuario, "tabgeneral", index);
 });
 
-cargaTorneosInd()
+cargaTorneosInd();
 
-        agregarEventListener()
-        USUARIOS.forEach((usuario,index) => {
-            crearTablaTorneoPrin(usuario, 'tabgeneral',index)
-        });
-        torneoSort()
-
+agregarEventListener();
+USUARIOS.forEach((usuario, index) => {
+  crearTablaTorneoPrin(usuario, "tabgeneral", index);
+});
+torneoSort();
 
 //Carga tabla individual al inicio//
 
-let torind = new Promise ((res, rej) => {
-    setTimeout(() => {
+let torind = new Promise((res, rej) => {
+  setTimeout(() => {
     res(document.getElementsByClassName("fstselected")[0].innerText);
     //document.getElementById(select).style.display = ''
-    },20);
-})
-torind.then((res) => document.getElementById(res).style.display = '')
-
-
+  }, 20);
+});
+torind.then((res) => (document.getElementById(res).style.display = ""));
 
 //////////AGREGAR JUGADORES TORNEOSIND///////////////////
 
-
-
-
-
-
-
 //////////EDITAR JUGADORES TORNEOSIND///////////////////
-let jugadoresTorneoInd = [{nombre: "carlos", id: 1}, {nombre: "julio", id: 2}, {nombre: "feli", id: 3}]
-
+let jugadoresTorneoInd = [
+  { nombre: "carlos", id: 1 },
+  { nombre: "julio", id: 2 },
+  { nombre: "feli", id: 3 },
+];
 
 const eliminarJugador = (id) => {
-    const result = jugadoresTorneoInd.filter((jug) => jug.id !== id)
-    jugadoresTorneoInd = result
-    document.getElementById('tbodyJugadores').innerHTML = ""
-    listaJugadoresTorneoInd(jugadoresTorneoInd)
+  const result = jugadoresTorneoInd.filter((jug) => jug.id !== id);
+  jugadoresTorneoInd = result;
+  document.getElementById("tbodyJugadores").innerHTML = "";
+  listaJugadoresTorneoInd(jugadoresTorneoInd);
+};
 
-}
-
-
-let table = document.createElement('table');
-let tbody = document.createElement('tbody');
-tbody.id = "tbodyJugadores"
+let table = document.createElement("table");
+let tbody = document.createElement("tbody");
+tbody.id = "tbodyJugadores";
 table.appendChild(tbody);
 
-document.getElementById('JugadoresTorneoIndEditar').appendChild(table);
+document.getElementById("JugadoresTorneoIndEditar").appendChild(table);
 
 function listaJugadoresTorneoInd(jugador) {
-    jugador.forEach((jugador) => {
-    let row_1 = document.createElement('tr');
-    let row_1_data_1 = document.createElement('td');
+  jugador.forEach((jugador) => {
+    let row_1 = document.createElement("tr");
+    let row_1_data_1 = document.createElement("td");
     row_1_data_1.innerHTML = jugador.nombre;
-    let row_1_data_2 = document.createElement('td');
-    row_1_data_2.innerHTML = `<i id="${jugador.id}" onclick="eliminarJugador(${jugador.id})" class="fa-regular fa-x"></i>` 
+    let row_1_data_2 = document.createElement("td");
+    row_1_data_2.innerHTML = `<i id="${jugador.id}" onclick="eliminarJugador(${jugador.id})" class="fa-regular fa-x"></i>`;
 
     row_1.appendChild(row_1_data_1);
     row_1.appendChild(row_1_data_2);
     tbody.appendChild(row_1);
-    })
-    };
+  });
+}
 
-    listaJugadoresTorneoInd(jugadoresTorneoInd)
+listaJugadoresTorneoInd(jugadoresTorneoInd);
 
+// function addItem(inputField) {
+
+//     var listItem = document.createElement("li");
+//     listItem.innerText = inputField.value; // passed the field.
+//     list.appendChild(listItem);
+//     return false; // stop submission
+//   }
+
+function agregarJugador(inputField) {
+  console.log(inputField.value);
+  jugadoresTorneoInd.push({ nombre: inputField.value, id: 0 });
+  document.getElementById("tbodyJugadores").innerHTML = "";
+  listaJugadoresTorneoInd(jugadoresTorneoInd);
+
+  return false; // stop submission
+}
+
+
+const jugadoresTorneo = []
+
+function agregarJugadorTorneo(nombre){
+    console.log(nombre)
+    jugadoresTorneo.push(nombre)
+    console.log(jugadoresTorneo)
+}
+
+
+function agregarTorneo(inputField) {
+    console.log(inputField)
+    const torneo = {nombre: inputField, jugadores: jugadoresTorneo}
+    console.log(torneo)
+
+  
+    return false; // stop submission
+  }
